23b8a3a79a532182d101c9e91df94fac21c5332e
Kirmanie L Ravariere <enamrik@gmail.com>
2025-05-13 19:38:27 -0400

    Add flow logs on subnets to better debug networking issues

--- infrastructure/lib/InfrastructureStack.ts
+++ infrastructure/lib/InfrastructureStack.ts
@@ -2,6 +2,8 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as logs from 'aws-cdk-lib/aws-logs';
+import * as iam from 'aws-cdk-lib/aws-iam';
 
 export class InfrastructureStack extends cdk.Stack {
   public readonly cluster: ecs.Cluster;
@@ -15,6 +17,22 @@ export class InfrastructureStack extends cdk.Stack {
       vpcName: this.vpcName,
       maxAzs: 2,
       natGateways: 1,
+    });
+
+    const flowLogGroup = new logs.LogGroup(this, 'VpcFlowLogGroup', {
+      retention: logs.RetentionDays.ONE_DAY,
+    });
+
+    const flowLogRole = new iam.Role(this, 'VpcFlowLogRole', {
+      assumedBy: new iam.ServicePrincipal('vpc-flow-logs.amazonaws.com'),
+    });
+
+    vpc.privateSubnets.forEach((subnet, index) => {
+      new ec2.FlowLog(this, `FlowLogPrivateSubnet${index}`, {
+        resourceType: ec2.FlowLogResourceType.fromSubnet(subnet),
+        trafficType: ec2.FlowLogTrafficType.ALL,
+        destination: ec2.FlowLogDestination.toCloudWatchLogs(flowLogGroup, flowLogRole),
+      });
     });
 
     this.cluster = new ecs.Cluster(this, 'SharedCluster', {
